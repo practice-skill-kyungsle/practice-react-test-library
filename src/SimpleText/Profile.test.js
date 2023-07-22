a3ebaf739c232d3288a0a1c91628d975940b101a
keinn51 <soogood97@gmail.com>
2023-07-23 00:42:43 +0900

    불필요 테스트 삭제

--- src/SimpleText/Profile.test.js
+++ src/SimpleText/Profile.test.js
@@ -3,10 +3,6 @@ import { render, screen } from "@testing-library/react";
 import Profile from "./Profile";
 
 describe("<Profile />", () => {
-    it("matches snapshot", () => {
-        const utils = render(<Profile username="kyungsle" name="이경수" />);
-        expect(utils.container).toMatchSnapshot();
-    });
     it("shows the props correctly", () => {
         render(<Profile username="kyungsle" name="이경수" />);
         const linkElement = screen.getByText(/kyungsle/i);
